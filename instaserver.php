a568874c296a024e830b3cc82e541806287e6936
Matteo Enna <info@matteoenna.it>
2017-12-27 19:53:30 +0100

    Create instaserver.php

---
+++ instaserver.php
@@ -0,0 +1,24 @@
+<?php
+    $paese = $_POST['search'];
+    $limit = $_POST['limit'];
+    if($_POST['key']!=='{TOKEN}') die('TOKEN FAIL');
+    
+    $insta_source = file_get_contents('https://www.instagram.com/explore/tags/'.trim($opt_search).'/'); // instagrame tag url
+    $shards = explode('window._sharedData = ', $insta_source);
+    $insta_json = explode(';</script>', $shards[1]); 
+    $insta_array = json_decode($insta_json[0], TRUE);
+    
+    $insta_array = $insta_array['entry_data']['TagPage'][0]['tag']['media']['nodes'];
+    $i = 1;
+    foreach ($insta_array as $k => $insta) {
+        if($i>$limit) continue;
+        $array[] = array(
+            'id'            =>  $insta['id'],
+            'code'          =>  $insta['code'],
+            'thumbnail_src' =>  $insta['thumbnail_src']
+        );
+        $i++;
+    }
+    echo json_encode($array);
+
+?>
